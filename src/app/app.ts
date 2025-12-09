6c9e8652781d22147b022d1dd91d873a754cb0cd
Luis Parente <parente.lgsp@outlook.com>
2025-12-09 08:10:20 -0300

    adicionado cabeçalho e rodapé

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CabecalhoComponent} from './componentes/cabecalho/cabecalho.component';
+import {RodapeComponent} from './componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-cabecalho',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './cabecalho.component.html',
+  styleUrl: './cabecalho.component.css',
+})
+export class CabecalhoComponent {
+
+}
